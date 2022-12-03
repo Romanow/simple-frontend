251147dc2ef7847b478d5d99b09f3c91204e6c3a
aromanov <aromanov@inno.tech>
2022-12-04 01:44:54 +0400

    add k8s deployment

--- src/Api.tsx
+++ src/Api.tsx
@@ -1,6 +1,8 @@
+const baseURL = process.env.REACT_APP_BACKEND_IP;
+
 export const Api = {
   message: async (): Promise<String> => {
-    return await fetch(`/backend/?person=docker`)
+    return await fetch(`${baseURL}?person=docker`)
     .then((response) => response.text())
   }
 }
